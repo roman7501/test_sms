066169fd00ea6bc7d5b2a9fd385dbaee43792868
Roman <romanjeanelie@yahoo.fr>
2020-08-05 15:31:15 +0200

    add sms function

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,4 +1,34 @@
 // submission-created.js
+// load the env file and make values aceessible via process.env
+
+require("dotenv").config();
+
+const {
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  CONTACT_NUMBERS,
+  BOT_NUMBER,
+  BOT_MESSAGE,
+} = process.env;
+
+// initialize the helper library client
+const client = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
 exports.handler = function (event, context, callback) {
-  console.log("Submission created !");
+  Promise.all(
+    // split the string of several messages into single numbers
+    // send message to each of them
+    CONTACT_NUMBERS.split(";").map((num) => {
+      return client.messages.create({
+        from: BOT_NUMBER,
+        to: num,
+        body: BOT_MESSAGE,
+      });
+    })
+  )
+    .then(() => callback(null, { statusCode: 200, body: "Created" }))
+    .catch((e) => {
+      console.log(e);
+      callback(e);
+    });
 };
